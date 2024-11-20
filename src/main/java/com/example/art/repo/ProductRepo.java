7c0bee94e52dbb4382b8c69e7f9fa86a2f1b6558
hrushikesh <hrushikeshnakka@gmail.com>
2024-11-20 16:22:02 +0530

    feat: added query to find top 2 product names whose price is in the range of 15 to 30

--- src/main/java/com/example/art/repo/ProductRepo.java
+++ src/main/java/com/example/art/repo/ProductRepo.java
@@ -2,7 +2,12 @@ package com.example.art.repo;
 
 import com.example.art.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
+    @Query(value="SELECT name FROM Product WHERE price BETWEEN 15 AND 30 ORDER BY price ASC LIMIT 2",nativeQuery = true)
+    List<String> top2in15and30price();
 
 }
